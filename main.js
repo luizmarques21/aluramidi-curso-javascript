c02a6dbcf07f32ea9262fe5d1d1080ea2c03348c
Luiz Marques <mariel.marques21@gmail.com>
2025-04-12 22:08:40 -0300

    Iterando em listas

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    console.log('');
+    document.querySelector(idElementoAudio).play();
 }
 
-const listaDeBotoes = document.querySelectorAll('.tecla');
-listaDeBotoes[0].onClick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+}
