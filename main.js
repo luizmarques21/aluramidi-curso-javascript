b2a3290abbda0d54eb6c586afc5912df555b488c
Luiz Marques <mariel.marques21@gmail.com>
2025-04-12 23:07:22 -0300

    Eventos e lógicas

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function tocaSom(idElementoAudio) {
-    console.log('');
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log("Elemento não encontrado");
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -13,4 +18,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
